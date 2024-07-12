f9d5753ec7b7cad05337e5520b9c40437005e8fb
Alessandro Ristori <81309500+RistoAle97@users.noreply.github.com>
2024-07-12 19:20:11 +0200

    Linter and formatter with pre-commit

--- cka/__init__.py
+++ cka/__init__.py
@@ -3,4 +3,5 @@ from .core import cka_base, cka_batch
 from .hsic import hsic0, hsic1
 from .utils import center_gram_matrix, linear_kernel, rbf_kernel
 
+
 __all__ = ["center_gram_matrix", "cka_base", "cka_batch", "hsic0", "hsic1", "linear_kernel", "rbf_kernel", "CKA"]
